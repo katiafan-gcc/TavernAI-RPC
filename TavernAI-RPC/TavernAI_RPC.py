084be07122944031d32774fdecf1afceb0d1f38e
time007gc <maksklimenko2001@gmail.com>
2023-02-23 22:57:21 -0500

    Going a different route, selenium seems better

--- TavernAI-RPC/TavernAI_RPC.py
+++ TavernAI-RPC/TavernAI_RPC.py
@@ -1,39 +1,39 @@
-import win32com.client
-import requests
-from bs4 import BeautifulSoup
-from pypresence import Presence
+import pyautogui
 import time
+from selenium import webdriver
+from pypresence import Presence
 
 # Initialize the Discord Rich Presence client
 client_id = '1078188469458841690'
 RPC = Presence(client_id)
 RPC.connect()
 
-# Create a web server that serves the HTML page containing the title tag
-# using Flask, for example:
-from flask import Flask, render_template
-app = Flask(__name__)
+RPC.update(state='Browsing the web', details='Chrome', large_image='chrome')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
-
-# Connect to the running Chrome browser
-chrome = win32com.client.Dispatch("ChromeTab.ChromeTabWrapper")
+# Initialize the Selenium WebDriver for Chrome
+driver = webdriver.Chrome()
 
 while True:
-    # Retrieve the HTML page containing the title tag
-    response = requests.get('http://127.0.0.1:8000')
-    soup = BeautifulSoup(response.text, 'html.parser')
+    # Get the title of the current Chrome tab
+    current_tab_title = pyautogui.getActiveWindowTitle()
+    print(pyautogui.getActiveWindowTitle())
 
-    # Extract the title tag from the HTML
-    title = soup.find('title').text
+    if current_tab_title == 'Tavern.AI - Google Chrome':
+        # Load the TavernAI website
+        driver.get('http://127.0.0.1:8000')
 
-    # Update the Discord Rich Presence status with the title of the web page
-    RPC.update(details=title)
+        # Wait for the page to load
+        time.sleep(5)
 
-    # Sleep for 15 seconds before checking for a new title
-    time.sleep(15)
+        # Get the name of the current channel on TavernAI
+        channel_name_element = driver.find_element_by_class_name('ch_name')
+        channel_name = channel_name_element.get_attribute('innerHTML').strip()
+
+        # Set the presence if the user is on the TavernAI tab
+        RPC.update(state='Playing TavernAI', details=f'In #{channel_name}', large_image='tavernai', small_image='playing')
+
+    time.sleep(5)
+
+# Close the Selenium WebDriver and RPC connection when done
+driver.quit()
+RPC.close()
