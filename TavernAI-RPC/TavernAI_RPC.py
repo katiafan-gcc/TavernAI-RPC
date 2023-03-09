59f3300a16a30388f40eead2d911574735d46dee
time007gc <maksklimenko2001@gmail.com>
2023-03-09 17:53:11 -0500

    Alright, this version works. Uploads the character to catbox.moe and then uses that to update RPC

--- TavernAI-RPC/TavernAI_RPC.py
+++ TavernAI-RPC/TavernAI_RPC.py
@@ -3,11 +3,14 @@ import socketserver
 from http import HTTPStatus
 import pypresence
 import json
-import time
+import os
+import requests
 
 client_id = "1078188469458841690"  # replace with your Discord application's client ID
 rpc = pypresence.Presence(client_id)
 rpc.connect()
+
+url = "https://catbox.moe/user/api.php"
 
 PORT = 8000
 
@@ -16,10 +19,19 @@ class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
         content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode())
-        ch_name = data.get('ch_name','')
-        avatar_str = "C:/Users/Maks/Downloads/newtavern/Tavern/TavernAI/public/characters/" + ch_name + ".png"
-        print(avatar_str)
-        rpc.update(details=ch_name, large_image=avatar_str)
+        ch_name = data.get('ch_name', '')
+        avatar_path = os.path.abspath("C:/Users/thatr/Documents/newtavern/Tavern/TavernAI/public/characters/" + ch_name + ".png")
+        avatar_url = "file:///" + avatar_path.replace("\\", "/")
+
+        with open(avatar_path, "rb") as f:
+            files = {"fileToUpload": f}
+            data = {"reqtype": "fileupload", "userhash": ""}
+            response = requests.post(url, data=data, files=files)
+
+        rpc.update(details="Talking to: " + ch_name, 
+                   large_image=response.text, 
+                   large_text="Probably fucking her"
+                   )
         self.send_response(HTTPStatus.OK)
         self.end_headers()
 
