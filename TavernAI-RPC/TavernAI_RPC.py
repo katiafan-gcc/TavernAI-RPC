9dc74de9dc5869921802bae3ffe358fec4637b36
time007gc <maksklimenko2001@gmail.com>
2023-03-09 19:06:00 -0500

    small changes, saves characters now to a dict. should probably use a file instead

--- TavernAI-RPC/TavernAI_RPC.py
+++ TavernAI-RPC/TavernAI_RPC.py
@@ -6,32 +6,48 @@ import json
 import os
 import requests
 
-client_id = "1078188469458841690"  # replace with your Discord application's client ID
+client_id = "1078188469458841690"
 rpc = pypresence.Presence(client_id)
 rpc.connect()
 
 url = "https://catbox.moe/user/api.php"
-
 PORT = 8000
+characters = {}
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
+        #getting the character name
         content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode())
         ch_name = data.get('ch_name', '')
-        avatar_path = os.path.abspath("C:/Users/thatr/Documents/newtavern/Tavern/TavernAI/public/characters/" + ch_name + ".png")
-        avatar_url = "file:///" + avatar_path.replace("\\", "/")
 
-        with open(avatar_path, "rb") as f:
-            files = {"fileToUpload": f}
-            data = {"reqtype": "fileupload", "userhash": ""}
-            response = requests.post(url, data=data, files=files)
+        #check if character exists already
+        if ch_name in characters:
+            rpc.update(details="Talking to: " + ch_name,
+                       large_image=characters.get(ch_name),
+                       large_text="Currently Roleplaying",
+                       small_image="tavern")
 
-        rpc.update(details="Talking to: " + ch_name, 
-                   large_image=response.text, 
-                   large_text="Probably fucking her"
-                   )
+        #otherwise do stuff
+        else:
+            avatar_path = os.path.abspath("C:/Users/thatr/Documents/newtavern/Tavern/TavernAI/public/characters/" + ch_name + ".png")
+            avatar_url = "file:///" + avatar_path.replace("\\", "/")
+
+            #send request to catbox.moe
+            with open(avatar_path, "rb") as f:
+                files = {"fileToUpload": f}
+                data = {"reqtype": "fileupload", "userhash": ""}
+                response = requests.post(url, data=data, files=files)
+
+            rpc.update(details="Talking to: " + ch_name, 
+                       large_image=response.text, 
+                       large_text="Currently Roleplaying",
+                       small_image="tavern")
+            
+            #save new character
+            characters[ch_name] = response.text
+
         self.send_response(HTTPStatus.OK)
         self.end_headers()
 
